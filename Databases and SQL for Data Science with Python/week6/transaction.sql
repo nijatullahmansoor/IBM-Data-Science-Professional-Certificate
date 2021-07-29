89aec503fee7f0f0290db41bca74e4686fcfb58a
nijatullahmansoor <55934107+nijatullahmansoor@users.noreply.github.com>
2021-07-29 14:22:33 +0530

    Add files via upload

---
+++ Databases and SQL for Data Science with Python/week6/transaction.sql
@@ -0,0 +1,37 @@
+DELIMITER $$
+ 
+ CREATE PROCEDURE TRANSACTION_ROSE()
+ LANGUAGE SQL           /*Language used in this routine */ 
+ MODIFIES SQL DATA      /*This routine will only write/modify data in the table*/ 
+ BEGIN
+		DECLARE SQLCODE INTEGER DEFAULT 0;                  -- Host variable SQLCODE declared and assigned 0
+        DECLARE retcode INTEGER DEFAULT 0;                  -- Local variable retcode with declared and assigned 0
+        DECLARE CONTINUE HANDLER FOR SQLEXCEPTION           -- Handler tell the routine what to do when an error or warning occurs
+        SET retcode = SQLCODE;                              -- Value of SQLCODE assigned to local variable retcode
+        
+        UPDATE BankAccounts
+        SET Balance = Balance-200
+        WHERE AccountName = 'Rose';
+        
+        UPDATE BankAccounts
+        SET Balance = Balance+200
+        WHERE AccountName = 'Shoe Shop';
+        
+        UPDATE ShoeShop
+        SET Stock = Stock-1
+        WHERE Product = 'Boots';
+        
+        UPDATE BankAccounts
+        SET Balance = Balance-300
+        WHERE AccountName = 'Rose';
+
+        
+        IF retcode < 0 THEN                                  --  SQLCODE returns negative value for error, zero for success, positive value for warning
+            ROLLBACK WORK;
+        
+        ELSE
+            COMMIT WORK;
+        
+        END IF;
+ END
+ $$
